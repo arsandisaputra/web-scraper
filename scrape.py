af1ffd4008da6008f97beeed99aff71774b7afa3
Arsandi Saputra <nyankoder@gmail.com>
2023-09-08 15:16:40 +0700

    Separate story to different files in output folder

--- scrape.py
+++ scrape.py
@@ -1,46 +1,105 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-  
-headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246", 'Accept':"application/json"}
+import string
+import os
+
+
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246", 'Accept': "application/json"}
 
 bookLangBtk = '5663693268320256'
 bookLangInd = '6260074016145408'
 bookIds = [
-    'f4392984-7a08-49e5-a4a4-56816f565d51',
-    '7c4652a5-1b86-4af4-adff-ea82ace515af',
-    # '2b226676-d3ca-41eb-860c-7fc44222751e',
-    # 'e4d01e0e-71c1-4d6a-b341-8bad028d0e7e',
-    # '7eb22438-84e9-4242-b86e-5ca5c6de3673',
-    # '841c89ef-b760-4df7-a46b-690fee90a3bc',
-    # '6826ae9d-b851-45c4-a93d-009a83101788',
-    # '2c5a7fee-76f5-4b16-95bf-19c261dce242',
-    # 'd40939ca-2c21-4145-8c72-d23365a21774',
-    # '7964ad44-55de-4e5f-9fea-deafcb916c9a',
-    # '58df69c5-1c1e-4575-b48e-3867e11c2054',
-    # '9ca2ada4-2456-467e-8229-d3f00cdf27fd',
-    # 'c7d00a36-0880-4c70-8c0f-b8111a43bbb7',
-    # '6c7caaa6-e667-476f-a5b6-508c7219d0b5',
-    # 'f1761a23-07a8-4490-b165-c1705489d3bc',
-    # 'bf1cb4d7-55dc-44ae-9941-a68548f1c0c7',
-    # 'd220da2d-f80e-4ee3-8e32-57abf8a40cd0',
+    '100c0cd9-dea4-469d-90bf-0057a7ae4aba',
+    '203e8d02-7768-4b16-8892-ff87ab2501d5',
+    '5eb939b0-2493-4dbb-b206-b4ccb6c2a648',
+    '434524bc-4284-444b-9091-784b311430ed',
+    '9d6d2a26-ead5-4a0b-88ff-87c0775046c7',
+    '7cc98d50-d522-47a7-a8e3-f48a3255b707',
+    '1c0431b0-ff43-4995-8a3d-711d3ff0b165',
+    'b722fa1d-4622-4f9d-828f-0a9ddedbd05a',
+    'ad99cff1-9c00-4da4-a23f-bf0a5c5a2287',
+    'cacbc648-f4a3-4d5c-94eb-aa925aa431e2',
+    '10f8d8af-3dac-4874-9582-bff4e41b1fbd',
+    'b2bcc483-536b-4368-a6f0-32a3011214cf',
+    '48f1a952-9aff-4a1a-9dfc-32dbffd4aa80',
+    '675274a3-e67e-4b35-b6a1-7498d960e28c',
+    '59874445-5f7c-40fe-a8f5-47ac380d4a86',
+    '4a5788cd-47bd-4c96-b357-e5c994b6c0b6',
+    'f8b23649-dc7b-4a21-b3db-4a97634e7275',
 ]
 # https://letsreadasia.org/api/v5/book/preview/language/5663693268320256/book/203e8d02-7768-4b16-8892-ff87ab2501d5
+folder = "output"
+if not os.path.exists(folder):
+    os.mkdir(folder)
+    print(f"Folder '{folder}' telah dibuat")
 
+translator = str.maketrans('', '', string.punctuation)
 bookContents = []
 for bookId in bookIds:
-    URL = "https://letsreadasia.org/api/v5/book/preview/language/"+bookLangInd+"/book/" + bookId
+    URL = "https://letsreadasia.org/api/v5/book/preview/language/" + \
+        bookLangBtk + "/book/" + bookId
     # URL = "https://letsreadasia.org/api/v5/book/preview/language/5663693268320256/book/203e8d02-7768-4b16-8892-ff87ab2501d5"
     # print(URL)
-#     URL_indo = "https://letsreadasia.org/api/v5/book/preview/language/"+bookLangInd+"/book/"+id
+    URL_indo = "https://letsreadasia.org/api/v5/book/preview/language/" + \
+        bookLangInd+"/book/"+bookId
     r_batak = requests.get(url=URL, headers=headers)
-#     r_indo = requests.get(url=URL_indo, headers=headers)
+    r_indo = requests.get(url=URL_indo, headers=headers)
     data_batak = json.loads(r_batak.content)
-#     data_indo = json.loads(r_indo.content)
-    print(URL)
-    print(data_batak)
-    # bookContents.append(data_batak['masterBookMetaData']['title']+'~'+data_indo['masterBookMetaData']['title'])
-#     bookContents.extend([BeautifulSoup(x['extractedLongContentValue'].rstrip('\n'), 'html.parser').get_text()+ '~'+ BeautifulSoup(y['extractedLongContentValue'].rstrip('\n'), 'html.parser').get_text()  for x, y in zip(data_batak['pages'], data_indo['pages'])])
+    data_indo = json.loads(r_indo.content)
+    bookContents.append(data_batak['name']+'~'+data_indo['name'])
+    pages_batak = {page['pageNum']: BeautifulSoup(page['extractedLongContentValue'].rstrip(
+        '\n'), 'html.parser').get_text() for page in data_batak['pages']}
+    pages_indo = {page['pageNum']: BeautifulSoup(page['extractedLongContentValue'].rstrip(
+        '\n'), 'html.parser').get_text() for page in data_indo['pages']}
+    if data_batak['description'] is not None and data_indo['description'] is not None:
+        pages_batak[0] = BeautifulSoup(
+            data_batak['description'].rstrip('\n'), 'html.parser').get_text()
+        pages_indo[0] = BeautifulSoup(
+            data_indo['description'].rstrip('\n'), 'html.parser').get_text()
 
+    pages_batak = dict(sorted(pages_batak.items(), key=lambda item: item[0]))
+    pages_indo = dict(sorted(pages_indo.items(), key=lambda item: item[0]))
+
+    title = data_batak['name'].translate(translator)
+    title = title.replace(' ', '_')
+
+    file_path = os.path.join(folder, title+"_batak.txt")
+    with open(file_path, 'w') as file:
+        for key, value in pages_batak.items():
+            file.write(value + "\n")
+
+    file_path = os.path.join(folder, title+"_indo.txt")
+    with open(file_path, 'w') as file:
+        for key, value in pages_indo.items():
+            file.write(value + "\n")
+
+    # if (len(pages_batak) != len(pages_indo)):
+    #     print(data_batak['name'])
+    #     print(URL)
+    #     print(URL_indo)
+    #     print('ERROR JUMLAH HALAMAN TIDAK SAMA!',
+    #           len(pages_batak), len(pages_indo))
+    #     print()
+    #     break
+
+    # bookContents.extend([xvalue+'~'+yvalue for xkey, xvalue, ykey,
+    #                     yvalue in zip(pages_batak.items(), pages_indo.items())])
+
+    # for i in range(1, min(len(pages_batak), len(pages_indo))-1):
+    #     print(pages_batak[i]+'~'+pages_indo[i])
+# print(bookContents)
+# print(data_batak[])
+# print(pages_batak)
+# file_path = "output.txt"
+# print(len(bookContents))
 # for x in bookContents:
-#     print(x)
+# with open(file_path, 'w') as file:
+#     # Write the string to the file
+#     for x in bookContents:
+#         file.write(x+"\n")
+#     # print(x)
+
+
+# # Open the file in write mode ('w' or 'wt' for text mode)
--- scrape_books.py
+++ scrape_books.py
@@ -1,8 +1,9 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-  
-headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246", 'Accept':"application/json"}
+
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246", 'Accept': "application/json"}
 
 cursors = ['', '1', '2']
 URL = "https://letsreadasia.org/api/book/elastic/search/?searchText=&lId=5663693268320256&countryOfOrigin=&audio=false&activity=false&altImage=false&limit=20&cursor="
@@ -13,9 +14,9 @@ for c in cursors:
     data_books = json.loads(r_list_books.content)
 
     for o in data_books['other']:
-        books[o['name']] = o['id']
+        books[o['name']] = o['masterBookId']
         # print(o['id']+' '+o['name'])
 sorted_books = dict(sorted(books.items()))
 # print(sorted_books)
 for key, value in sorted_books.items():
-    print(value +' '+ key)
+    print(value)
